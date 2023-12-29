f507318fc6620912e6fc7612b4e1573c84154263
skarab42 <skarab@bluewin.ch>
2023-12-29 08:05:53 +0100

    feat: add `unicorn` plugin

--- source/create-flat-config.ts
+++ source/create-flat-config.ts
@@ -2,12 +2,13 @@ import { ecmascript } from './configs/ecmascript.js';
 import { common } from './configs/common.js';
 import type { FlatConfig } from './common/types.js';
 import { packageExists } from './common/package-exists.js';
+import { unicorn } from './configs/unicorn.js';
 
 export async function createFlatConfig(): Promise<FlatConfig[]> {
   const ts = packageExists('typescript');
   const jsx = false;
 
-  const configs = [common(), ecmascript({ filesOptions: { ts, jsx } })];
+  const configs = [common(), ecmascript({ filesOptions: { ts, jsx } }), unicorn({ filesOptions: { ts, jsx } })];
 
   if (ts) {
     const { typescript } = await import('./configs/typescript.js');
